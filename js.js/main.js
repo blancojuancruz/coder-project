70f4287450dd3e864ff147578fb42d7a3659976a
blancojuancruz <99100776+blancojuancruz@users.noreply.github.com>
2022-03-09 22:46:19 -0300

    toast

--- js.js/main.js
+++ js.js/main.js
@@ -3,7 +3,6 @@ var toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
   toastTrigger.addEventListener('click', function () {
     var toast = new bootstrap.Toast(toastLiveExample)
-
     toast.show()
   })
 }
